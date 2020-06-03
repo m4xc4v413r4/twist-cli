4461c7ba57fa8eb93da2b80ee904c5580b46d3e7
Wither OrNot <damemem@gmail.com>
2020-06-02 22:59:29 -0400

    interactive mode

---
+++ twist_api.py
@@ -0,0 +1,93 @@
+import requests
+from cryptojs_aes_port import decrypt
+import subprocess
+import os
+import code
+import sys
+from tempfile import TemporaryFile
+from tqdm import tqdm
+
+API_KEY = "1rj2vRtegS8Y60B3w3qNZm5T2Q0TN2NR"
+AES_KEY = b"LXgIVP&PorO68Rq7dTx8N^lP!Fa5sGJ^*XK"
+
+def api_request(url):
+    r = requests.get(
+        f"https://twist.moe/api{url}",
+        headers = {
+            "x-access-token": API_KEY
+        }
+    )
+
+    r.raise_for_status()
+    return r.json()
+
+def get_shows():
+    shows_json = api_request("/anime")
+    shows = {
+        i["title"]: i["slug"]["slug"] for i in shows_json
+    }
+
+    return shows
+
+def get_source(slug, ep_number):
+    sources = api_request(f"/anime/{slug}/sources")
+    encrypted_url = list(filter(lambda ep: ep["number"] == ep_number, sources))[0]["source"]
+    url = decrypt(AES_KEY, encrypted_url)
+    return f"https://twistcdn.bunny.sh{url}"
+
+def get_num_episodes(slug):
+    sources = api_request(f"/anime/{slug}/sources")
+    return len(sources)
+
+def download(slug, ep_numbers, out=None):
+    for ep in ep_numbers:
+        out = out if out else f"{slug}-{ep}.mp4"
+        print(f"Downloading episode {ep} to {out}...")
+        url = get_source(slug, ep)
+
+        r = requests.get(
+            url,
+            headers={
+                "Referer": "https://twist.moe/"
+            },
+            stream=True
+        )
+        r.raise_for_status()
+        
+        with open(out, "wb") as f:
+            for chunk in tqdm(r.iter_content(), total=int(r.headers["Content-Length"]), unit="B", unit_scale=True):
+                f.write(chunk)
+    
+    print("Finished!")
+
+def stream(slug, ep_number):
+    killed_by_ctrl_c = False
+    url = get_source(slug, ep_number)
+
+    r = requests.get(
+        url,
+        headers={
+            "Referer": "https://twist.moe/"
+        },
+        stream=True
+    )
+    r.raise_for_status()
+    p = subprocess.Popen(["mplayer", "-really-quiet", "-msglevel", "all=-1", "-cache", "32768", "-"], stdin=subprocess.PIPE, stderr=TemporaryFile())
+
+    try:
+        for chunk in r.iter_content(32768):
+            p.stdin.write(chunk)
+    except TypeError:
+        killed_by_ctrl_c = True
+    except KeyboardInterrupt:
+        killed_by_ctrl_c = True
+    except Exception as e:
+        pass
+
+    p.kill()
+    
+    return killed_by_ctrl_c
+
+if __name__ == "__main__":
+    # download("kobayashi-san-chi-no-maid-dragon", [1])
+    stream("kobayashi-san-chi-no-maid-dragon", 6)
---
+++ twist_cli.py
@@ -0,0 +1,143 @@
+import curses
+import string
+import signal
+import sys
+from twist_api import download, stream, get_num_episodes, get_shows
+
+signal.signal(signal.SIGINT, exit)
+
+def init_window():
+    screen = curses.initscr()
+    curses.noecho()
+    curses.nocbreak()
+    screen.keypad(True)
+    curses.curs_set(0)
+    return screen
+
+def exit_window(screen):
+    curses.curs_set(1)
+    curses.cbreak()
+    screen.keypad(False)
+    curses.echo()
+    curses.endwin()
+
+def stream_menu(screen: curses.window, show, slug, num_episodes):
+    selected_index = 0
+    playing_index = None
+    shift = 0
+    ymax, xmax = screen.getmaxyx()
+
+    while True:
+        screen.clear()
+        screen.addstr(0, 0, f"Starting episode? {selected_index + 1 + shift}")
+        screen.addstr(1, 0, f"{show}:", curses.A_BOLD)
+        max_index = shift + ymax - 1
+        num_lines = min(num_episodes, ymax-2)
+        screen.addstr(0, 35, f"{max_index} {num_episodes} {shift} {ymax}")
+
+        for i in range(num_lines):
+            try:
+                if i == selected_index:
+                    screen.addstr(i+2, 0, f"Episode {i + 1 + shift}", curses.A_REVERSE)
+                else:
+                    screen.addstr(i+2, 0, f"Episode {i + 1 + shift}")
+            except:
+                raise Exception(f"{i} {shift} {i+2-shift} {num_lines}")
+        
+        try:
+            c = screen.getch()
+        except:
+            exit()
+        
+        if c == curses.KEY_UP:
+            if selected_index > 0:
+                selected_index -= 1
+            elif shift > 0:
+                shift -= 1 
+        elif c == curses.KEY_DOWN:
+            if selected_index < ymax - 3:
+                selected_index += 1
+            elif max_index < num_episodes:
+                shift += 1
+        elif c in (10, curses.KEY_ENTER):
+            start_stream(screen, slug, selected_index + 1 + shift, num_episodes)
+        elif chr(c) == "b":
+            return False
+
+        screen.refresh()
+
+def start_stream(screen: curses.window, slug, ep_start, num_episodes):
+    screen.clear()
+    
+    for i in range(ep_start, num_episodes+1):
+        screen.addstr(0, 0, f"Playing episode {i}, press Ctrl+C to quit")
+        screen.refresh()
+        if stream(slug, i):
+            exit()
+
+def main(screen: curses.window):
+    curses.curs_set(0)
+    search_term = ""
+    selected_index = 0
+    shift = 0
+    ymax, xmax = screen.getmaxyx()
+    shows = get_shows()
+
+    while True:
+        screen.clear()
+        screen.addstr(0, 0, "Search: "+search_term, curses.A_BOLD)
+        max_index = shift+ymax-2
+        matches = list(filter(lambda i: search_term.lower() in i.lower(), shows.keys()))
+        visible_matches = list(enumerate(matches[shift:max_index+1]))
+        max_selection = len(visible_matches) - 1
+
+        if max_selection < ymax-2 and shift > 0:
+            shift = 0
+            continue
+
+        if selected_index > max_selection:
+            selected_index = max_selection
+
+        if selected_index == -1:
+            selected_index = 0
+
+        for i, show in visible_matches:
+            try:
+                if i == selected_index:
+                    screen.addstr(i+1, 0, show, curses.A_REVERSE)
+                else:
+                    screen.addstr(i+1, 0, show)
+            except:
+                raise Exception(f"{i} {ymax}")
+
+        try:
+            c = screen.getch()
+        except:
+            exit()
+        
+        if c == curses.KEY_UP:
+            if selected_index > 0:
+                selected_index -= 1
+            elif shift > 0:
+                shift -= 1 
+        elif c == curses.KEY_DOWN:
+            if selected_index < max_selection:
+                selected_index += 1
+            elif max_index+1 < len(matches):
+                shift += 1
+        elif c in (10, curses.KEY_ENTER):
+            _, show = visible_matches[selected_index]
+            slug = shows[show]
+            num_episodes = get_num_episodes(slug)
+            if stream_menu(screen, show, slug, num_episodes):
+                break
+        elif c == curses.KEY_BACKSPACE:
+            search_term = search_term[:-1]
+        elif chr(c) == "\t":
+            _, show = visible_matches[selected_index]
+            search_term = show
+        elif chr(c) in string.printable:
+            search_term += chr(c)
+
+        screen.refresh()
+
